60be5a101cf483c4bf05ea5e65b2eebcb0342c54
lilkr <maintiendrai@gmail.com>
2016-02-12 23:24:56 +0800

    add fm model

---
+++ DoraCMSSDK/DoraCMSSDK/Models/CMSSDKFMModel.h
@@ -0,0 +1,13 @@
+//
+//  CMSSDKFMModel.h
+//  DoraCMSSDK
+//
+//  Created by Diana on 2/12/16.
+//  Copyright © 2016 maintiendrai. All rights reserved.
+//
+
+#import "CMSSDKModel.h"
+
+@interface CMSSDKFMModel : CMSSDKModel
+
+@end
---
+++ DoraCMSSDK/DoraCMSSDK/Models/CMSSDKFMModel.m
@@ -0,0 +1,13 @@
+//
+//  CMSSDKFMModel.m
+//  DoraCMSSDK
+//
+//  Created by Diana on 2/12/16.
+//  Copyright © 2016 maintiendrai. All rights reserved.
+//
+
+#import "CMSSDKFMModel.h"
+
+@implementation CMSSDKFMModel
+
+@end
